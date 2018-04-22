e061aa51ee431db380aa738da613582da4bd366b
stwa <s68772@beuth-hochschule.de>
2018-04-22 13:55:11 +0200

    implemented task 2.1 and task 2.2

--- uebung1/aufgabe2/aufg2.py
+++ uebung1/aufgabe2/aufg2.py
@@ -1,35 +1,68 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('resources/images/lenna.jpg', 1)  # 1 = farbe, 0 = grau (mittelwert von rgb) und -1 = with alphachannel
-img2 = cv2.imread('resources/images/lenna.jpg', 1)  # 1 = farbe, 0 = grau (mittelwert von rgb) und -1 = with alphachannel
-img_grey = cv2.imread('resources/images/lenna.jpg', 0)
-#print(img.shape[:2])  # Anzahl Reihe und Spalte
-
-i=0
-for row in img_grey:
-    j=0
-    for col in row:
-        img2[i][j][0] = col
-        img2[i][j][1] = col
-        img2[i][j][2] = col
-        j+=1
-    i+=1
-
-
-bothImg = np.concatenate((img2, img), axis=1) # 1=vertikal 0 = horizontal
-#cv2.imwrite('resources/images/lenna_both.png',vis)
 
 def translation(img, value=10):
-    num_rows, num_cols = bothImg.shape[:2]
+    num_rows, num_cols = img.shape[:2]
     translation_matrix = np.float32([[1,0,value], [0,1,0]])
-    img_translation = cv2.warpAffine(bothImg, translation_matrix, (num_cols, num_rows))
-    cv2.imshow('Translation', img_translation)
+    img_translation = cv2.warpAffine(img, translation_matrix, (num_cols, num_rows))
 
+    return img_translation;
+    #cv2.imshow('Translation', img_translation)
 
+def rotation(img, angle=10):
+  image_center = tuple(np.array(img.shape[1::-1]) / 2)
+  rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+  img_rotation = cv2.warpAffine(img, rot_mat, img.shape[1::-1], flags=cv2.INTER_LINEAR)
 
+  return img_rotation
 
-#cv2.waitKey()
-cv2.imshow('bothImg', bothImg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def prepareGreyImage(img, img_grey):
+    i = 0
+    for row in img_grey:
+        j = 0
+        for col in row:
+            img[i][j][0] = col
+            img[i][j][1] = col
+            img[i][j][2] = col
+            j += 1
+        i += 1
+
+    return img
+
+def mergeImages(img, img2):
+
+    return np.concatenate((img, img2), axis=1) # 1=vertikal 0 = horizontal
+
+# -- MainProgramm ---
+def main():
+    img_orig = cv2.imread('resources/images/lenna.jpg', 1)  # 1 = farbe, 0 = grau (mittelwert von rgb) und -1 = with alphachannel
+    img2_orig = cv2.imread('resources/images/lenna.jpg', 1)  # 1 = farbe, 0 = grau (mittelwert von rgb) und -1 = with alphachannel
+    img_grey = cv2.imread('resources/images/lenna.jpg', 0)
+    #print(img.shape[:2])  # Anzahl Reihe und Spalte
+
+    img_grey = prepareGreyImage(img2_orig, img_grey)
+    bothImg = mergeImages(img_grey, img_orig)
+
+    #cv2.imwrite('resources/images/lenna_both.png',vis) # save image
+
+    translationValue = 0;
+    rotationValue = 0;
+    cv2.imshow('Image', bothImg)
+    while (1):
+        key = cv2.waitKey(0)
+        print(key)
+        if key == 113:       #ascii value for q
+            print('q')
+            cv2.destroyAllWindows()
+            break
+        elif key == 116:         #ascii value for t
+            translationValue +=10;
+            bothImg = translation(bothImg, translationValue)
+            cv2.imshow('Image', bothImg)
+        elif key == 114:    #ascii value for t
+            rotationValue += 10;
+            bothImg = rotation(bothImg, rotationValue)
+            cv2.imshow('Image', bothImg)
+
+main()
