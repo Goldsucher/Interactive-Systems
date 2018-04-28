fix translation and rotation bug and started with tasks 2.3 and 2.4

uebung1/aufgabe2/aufg2.py
@@ -1,15 +1,17 @@
+# Diese Code beinhaltet Aufgaben 2.1 und 2.2 vom Übungsblatt 1
+# Stephan Wagner s853668
 import numpy as np
 import cv2
 
-
+# https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_geometric_transformations/py_geometric_transformations.html
 def translation(img, value=10):
     num_rows, num_cols = img.shape[:2]
     translation_matrix = np.float32([[1,0,value], [0,1,0]])
     img_translation = cv2.warpAffine(img, translation_matrix, (num_cols, num_rows))
 
     return img_translation;
-    #cv2.imshow('Translation', img_translation)
 
+# https://stackoverflow.com/questions/9041681/opencv-python-rotate-image-by-x-degrees-around-specific-point
 def rotation(img, angle=10):
   image_center = tuple(np.array(img.shape[1::-1]) / 2)
   rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
@@ -37,31 +39,33 @@ def mergeImages(img, img2):
 # -- MainProgramm ---
 def main():
     img_orig = cv2.imread('resources/images/lenna.jpg', 1)  # 1 = farbe, 0 = grau (mittelwert von rgb) und -1 = with alphachannel
-    img2_orig = cv2.imread('resources/images/lenna.jpg', 1)  # 1 = farbe, 0 = grau (mittelwert von rgb) und -1 = with alphachannel
+    img2_orig = cv2.imread('resources/images/lenna.jpg', 1)
     img_grey = cv2.imread('resources/images/lenna.jpg', 0)
     #print(img.shape[:2])  # Anzahl Reihe und Spalte
 
     img_grey = prepareGreyImage(img2_orig, img_grey)
     bothImg = mergeImages(img_grey, img_orig)
 
-    #cv2.imwrite('resources/images/lenna_both.png',vis) # save image
+    #cv2.imwrite('resources/images/lenna_both.jpg',vis) # save image
 
     translationValue = 0;
     rotationValue = 0;
     cv2.imshow('Image', bothImg)
     while (1):
+        bothImg = mergeImages(img_grey, img_orig)
         key = cv2.waitKey(0)
-        print(key)
+        # https://stackoverflow.com/questions/14494101/using-other-keys-for-the-waitkey-function-of-opencv
         if key == 113:       #ascii value for q
-            print('q')
             cv2.destroyAllWindows()
             break
         elif key == 116:         #ascii value for t
             translationValue +=10;
             bothImg = translation(bothImg, translationValue)
+            bothImg = rotation(bothImg, rotationValue)
             cv2.imshow('Image', bothImg)
         elif key == 114:    #ascii value for t
             rotationValue += 10;
+            bothImg = translation(bothImg, translationValue)
             bothImg = rotation(bothImg, rotationValue)
             cv2.imshow('Image', bothImg)
 