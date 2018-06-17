9536ad340f284898d86747f8dca76e6e01b4e8f7
gilly <gilly@gmx.net>
2018-06-17 16:24:07 +0200

    abgabe rdy

--- 3_tracking/3_tracking_1.py
+++ 3_tracking/3_tracking_1.py
@@ -1,36 +1,135 @@
 import cv2
+import numpy as np
+
+
+def match_keypoints(kps1, kps2, desc1, desc2):
+    """This function computes the matching of image features between two different
+    images and a transformation matrix (aka homography) that we will use to unwarp the images
+    and place them correctly next to each other. There is no need for modifying this, we will
+    cover what is happening here later in the course.
+    """
+
+    # match ratio to clean feature area from non-important ones
+    distanceRatio = 0.75
+    # theshold for homography
+    reprojectionThreshold = 4.0
+
+    # compute the raw matches using a Bruteforce matcher that
+    # compares image descriptors/feature vectors in high-dimensional space
+    # by employing K-Nearest-Neighbor match (more next course)
+    bf = cv2.BFMatcher()
+    rawmatches = bf.knnMatch(desc1, desc2, 2)
+    matches = []
+
+    # loop over the raw matches and filter them
+    for m in rawmatches:
+        # ensure the distance is within a certain ratio of each
+        # other (i.e. David Lowe's ratio = 0.75)
+        # in other words - panorama images need some useful overlap
+        if len(m) == 2 and m[0].distance < m[1].distance * distanceRatio:
+            matches.append((m[0].trainIdx, m[0].queryIdx))
+
+    # we need to compute a homography - more next course
+    # computing a homography requires at least 4 matches
+    if len(matches) > 4:
+        # construct the two sets of points
+        ptsPano1 = np.float32([kps1[i].pt for (_, i) in matches])
+        ptsPano2 = np.float32([kps2[i].pt for (i, _) in matches])
+
+        # compute the homography between the two sets of points
+        (H, status) = cv2.findHomography(ptsPano1, ptsPano2, cv2.RANSAC, reprojectionThreshold)
+
+        # we return the corresponding perspective transform and some
+        # necessary status object + the used matches
+        return (H, status, matches)
+
+
+def draw_matches(img1, img2, kp1, kp2, matches, status):
+    """For each pair of points we draw a line between both images and circles,
+    then connect a line between them.
+    Returns a new image containing the visualized matches
+    """
+
+    (h1, w1) = img1.shape[:2]
+    (h2, w2) = img2.shape[:2]
+    vis = np.zeros((max(h1, h2), w1 + w2, 3), dtype="uint8")
+    vis[0:h1, 0:w1] = img1
+    vis[0:h2, w1:] = img2
+
+    for ((idx2, idx1), s) in zip(matches, status):
+        # only process the match if the keypoint was successfully matched
+        if s == 1:
+
+            # x - columns
+            # y - rows
+            (x1, y1) = kp1[idx1].pt
+            (x2, y2) = kp2[idx2].pt
+
+            # Draw a small circle at both co-ordinates
+            # radius 4
+            # colour blue
+            # thickness = 1
+            cv2.circle(vis, (int(x1), int(y1)), 4, (255, 255, 0), 1)
+            cv2.circle(vis, (int(x2) + w1, int(y2)), 4, (255, 255, 0), 1)
+
+            # Draw a line in between the two points
+            # thickness = 1
+            # colour blue
+            cv2.line(vis, (int(x1), int(y1)), (int(x2) + w1, int(y2)), (255, 0, 0), 1)
+
+    return vis
+
 
 cap = cv2.VideoCapture(0)
-cv2.namedWindow('Interactive Systems: Towards AR Tracking')
+
+# initialize marker image
+img_marker_color = cv2.imread('resources/images/marker.jpg', 1)
+img_marker_grey = cv2.cvtColor(img_marker_color, cv2.COLOR_BGR2GRAY)
+
+# initialize SIFT
+sift = cv2.xfeatures2d.SIFT_create()
+
+# find keypoints and discriptor for marker image
+kp_marker, descr_marker = sift.detectAndCompute(img_marker_grey, None)
 
 while True:
-    # 1. read each frame from the camera (if necessary resize the image)
-    #    and extract the SIFT features using OpenCV methods
-    #    Note: use the gray image - so you need to convert the image
-    # 2. draw the keypoints using cv2.drawKeypoints
-    #    There are several flags for visualization - e.g. DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
-
-    # close the window and application by pressing a key
 
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    ret, img_cam_color = cap.read()
+    img_cam_grey = cv2.cvtColor(img_cam_color, cv2.COLOR_BGR2GRAY)
 
-    # Our operations on the frame come here
-    img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    img = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
-
-    sift = cv2.xfeatures2d.SIFT_create()
-    kp = sift.detect(img, None)
-
-    img = cv2.drawKeypoints(img, kp, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    cv2.imshow('', img)
-
-    # leave loog on key press 'q'
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
+    key = cv2.waitKey(50)
+    if key & 0xFF == ord('q'):
         break
 
-# clean up
+    # find keypoints and discriptor for cam frame
+    kp_cam, descr_cam = sift.detectAndCompute(img_cam_grey, None)
+
+    if kp_cam is not None and descr_cam is not None:
+        # find matches
+        H, status, matches = match_keypoints(kp_marker, kp_cam, descr_marker, descr_cam)
+
+        if status is not None and matches is not None:
+            # draw matches
+            img_result = draw_matches(img_marker_color, img_cam_color, kp_marker, kp_cam, matches, status)
+        elif matches is None:
+            print("No matches were found. Program was terminated")
+            break
+        elif status is None:
+            print("No status was found. Program was terminated")
+            break
+
+    elif kp_cam is None :
+        print("No keypoints were found for cam frame . Program was terminated")
+        break
+    elif descr_cam is None:
+        print("No descriptors were found for cam frame. Program was terminated")
+        break
+
+    # show result
+    cv2.imshow('Marker / Cam', img_result)
+
+
+# Destroy Windows and exit the program
 cap.release()
 cv2.destroyAllWindows()
---
+++ 3_tracking/3_tracking_4.py
@@ -0,0 +1,171 @@
+# Diese Code beinhaltet Aufgabe 4 vom Übungsblatt 3
+# Stephan Wagner s853668
+
+import cv2
+import numpy as np
+
+# global constants
+min_matches = 10
+
+# initialize flann and SIFT extractor
+# note unfortunately in the latest OpenCV + python is a minor bug in the flann
+# flann = cv2.FlannBasedMatcher(indexParams, {})
+# so we use the alternative but slower Brute-Force Matcher BFMatcher
+sift = cv2.xfeatures2d.SIFT_create()
+bfMatcher = cv2.BFMatcher()
+
+# extract marker descriptors
+img_marker_color = cv2.imread('resources/images/marker.jpg', 1)
+kp_marker, descr_marker = sift.detectAndCompute(img_marker_color, None)
+
+def render_virtual_object(img, x_start, y_start, x_end, y_end, quad):
+    # define vertices, edges and colors of your 3D object, e.g. cube
+    z = 1
+    vertices = np.float32([[0, 0, 0],
+                           [1, 0, 0],
+                           [1, 1, 0],
+                           [0, 1, 0],
+                           [0, 0, z],
+                           [1, 0, z],
+                           [1, 1, z],
+                           [0, 1, z]])
+
+    edges = [(0, 1),
+             (1, 2),
+             (2, 3),
+             (3, 0),
+
+             (4, 5),
+             (5, 6),
+             (6, 7),
+             (7, 4),
+
+             (0, 4),
+             (1, 5),
+             (2, 6),
+             (3, 7)]
+
+    color_lines = (0, 0, 0)
+
+    # define quad plane in 3D coordinates with z = 0
+    quad_3d = np.float32([[x_start, y_start, 0], [x_end, y_start, 0],
+                          [x_end, y_end, 0], [x_start, y_end, 0]])
+
+    h, w = img.shape[:2]
+    # define intrinsic camera parameter
+    K = np.float64([[w, 0, 0.5 * (w - 1)],
+                    [0, w, 0.5 * (h - 1)],
+                    [0, 0, 1.0]])
+
+    # find object pose from 3D-2D point correspondences of the 3d quad using Levenberg-Marquardt optimization
+    # in order to work we need K (given above and YOUR distortion coefficients from Assignment 2 (camera calibration))
+    dist_coef = np.array([0.06489938,  0.2053827,   0.00292677,  0.00300208, -1.12425709])
+
+    # compute extrinsic camera parameters using cv2.solvePnP
+    _, rvec, tvec = cv2.solvePnP(quad_3d, quad, K, dist_coef, flags=cv2.SOLVEPNP_ITERATIVE)
+    # transform vertices: scale and translate form 0 - 1, in window size of the marker
+    scale = [x_end - x_start, y_end - y_start, x_end - x_start]
+    trans = [x_start, y_start, -x_end - x_start]
+
+    verts = scale * vertices + trans
+
+    # call cv2.projectPoints with verts, and solvePnP result, K, and dist_coeff
+    # returns a tuple that includes the transformed vertices as a first argument
+    verts, _ = cv2.projectPoints(verts, rvec, tvec, K, dist_coef)
+
+    # we need to reshape the result of projectPoints
+    verts = verts.reshape(-1, 2)
+
+    # render edges
+    for i, j in edges:
+        (x_start, y_start), (x_end, y_end) = verts[i], verts[j]
+        cv2.line(img, (int(x_start), int(y_start)), (int(x_end), int(y_end)), color_lines, 4)
+
+cap = cv2.VideoCapture(0)
+cv2.namedWindow('Interactive Systems: AR Tracking')
+while True:
+
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    key = cv2.waitKey(50)
+    if key & 0xFF == ord('q'):
+        break
+
+    # detect and compute descriptor in camera image
+    # and match with marker descriptor
+    kp_cam, descr_cam = sift.detectAndCompute(frame, None)
+    if descr_marker is None or descr_cam is None:
+        print("No Descriptor found")
+        continue
+
+    matches = bfMatcher.knnMatch(descr_cam, descr_marker, 2)
+
+    # filter matches by distance [Lowe2004]
+    matches = [match[0] for match in matches if len(match) == 2 and
+               match[0].distance < match[1].distance * 0.75]
+
+    # if there are less than min_matches we just keep going looking
+    # early break
+    if len(matches) < min_matches:
+        cv2.imshow('Interactive Systems: AR Tracking', frame)
+        print("not enough matches")
+        continue
+
+    # extract 2d points from matches data structure
+    p0 = [kp_marker[m.trainIdx].pt for m in matches]
+    p1 = [kp_cam[m.queryIdx].pt for m in matches]
+
+    # transpose vectors
+    p0, p1 = np.array([p0, p1])
+
+    # we need at least 4 match points to find a homography matrix
+    if len(p0) < 4:
+        cv2.imshow('Interactive Systems: AR Tracking', frame)
+        print("less than 4 matches points  - no homography matrix found")
+        continue
+
+    # find homography using p0 and p1, returning H and status
+    # H - homography matrix
+    # status - status about inliers and outliers for the plane mapping
+    (H, mask) = cv2.findHomography(p0, p1, cv2.RANSAC, 4.0)
+
+    # on the basis of the status object we can now filter RANSAC outliers
+    mask = mask.ravel() != 0
+    if mask.sum() < min_matches:
+        cv2.imshow('Interactive Systems: AR Tracking', frame)
+        continue
+
+    # take only inliers - mask of Outlier/Inlier
+    p0, p1 = p0[mask], p1[mask]
+
+    # get the size of the marker and form a quad in pixel coords np float array using w/h as the corner points
+    h1, w1 = img_marker_color.shape[:2]
+    quad = [np.array([0, 0], dtype=np.float32),
+            np.array([h1, 0], dtype=np.float32),
+            np.array([w1, h1], dtype=np.float32),
+            np.array([0, w1], dtype=np.float32),]
+
+    print(quad)
+
+    # perspectiveTransform needs a 3-dimensional array
+    quad = np.array([quad])
+    quad_transformed = cv2.perspectiveTransform(quad, H)
+
+    # transform back to 2D array
+    quad = quad_transformed[0]
+
+    # render quad in image plane and feature points as circle using cv2.polylines + cv2.circle
+    cv2.polylines(frame, [quad.astype(dtype=np.int)], isClosed=True, color=(0, 0, 255), thickness=2)
+    for fp1 in p1:
+        fp1 = fp1.astype(np.int)
+        cv2.circle(frame, (fp1[0], fp1[1]), 10, (0, 0, 255))
+
+    # render virtual object on top of quad
+    render_virtual_object(frame, 0, 0, h1, w1, quad)
+
+    cv2.imshow('Interactive Systems: AR Tracking', frame)
+
+# Destroy Windows and exit the program
+cap.release()
+cv2.destroyAllWindows()
