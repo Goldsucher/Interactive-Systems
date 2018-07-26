561aa3e257aaf46354730d26cb83272b10a1a03e
gilly <gilly@gmx.net>
2018-07-26 22:27:52 +0200

    abgabe rdy sudoku

---
+++ 5_sudoku/models/cnnmodel.py
@@ -0,0 +1,35 @@
+from keras.models import Sequential
+from keras.layers import Dropout, MaxPooling2D, Flatten, Dense, Conv2D
+
+
+class CNNModel:
+    img_rows = 28
+    img_cols = 28
+
+    @staticmethod
+    def load_inputshape():
+        return CNNModel.img_rows, CNNModel.img_cols, 1
+
+    @staticmethod
+    def reshape_input_data(x_train, x_test):
+        x_train = x_train.reshape(x_train.shape[0], CNNModel.img_rows, CNNModel.img_cols, 1)
+        x_test = x_test.reshape(x_test.shape[0], CNNModel.img_rows, CNNModel.img_cols, 1)
+        return x_train, x_test
+
+
+    @staticmethod
+    def load_model(classes=10):
+        model = Sequential()
+        model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same', input_shape=(28, 28, 1)))
+        model.add(Conv2D(filters=32, kernel_size=1, activation='relu'))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'))
+        model.add(Conv2D(filters=64, kernel_size=1, activation='relu'))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Dropout(0.25))
+        model.add(Flatten())
+        model.add(Dense(units=512, activation='relu'))
+        model.add(Dense(units=classes, activation='softmax'))
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return model
---
+++ 5_sudoku/train_and_save_model.py
@@ -0,0 +1,44 @@
+from keras.utils import np_utils
+from models.cnnmodel import CNNModel
+from keras.datasets import mnist
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+print("Training matrix shape", X_train.shape)
+print("Testing matrix shape", X_test.shape)
+
+# image size
+img_rows, img_cols = 28, 28
+# count of classes 10: 0...9
+nb_classes = 10
+
+# converts a class vector (list of labels in one vector (as for SVM)
+# to binary class matrix (one-n-encoding)
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+# we need to reshape the input data to fit keras.io input matrix format
+X_train, X_test = CNNModel.reshape_input_data(X_train, X_test)
+
+# hyperparameter
+nb_epoch = 1
+batch_size = 128
+
+model = CNNModel.load_model(nb_classes)
+history = model.fit(X_train, Y_train,
+                    batch_size=batch_size,
+                    epochs=nb_epoch, verbose=1,
+                    validation_data=(X_test, Y_test))
+
+score = model.evaluate(X_test, Y_test, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
+# save model
+weights_fn = "models/weights" + str(nb_epoch) + ".h5"
+print("saving model to "+weights_fn)
+model.save_weights(weights_fn)
